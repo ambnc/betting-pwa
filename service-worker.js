bf4c1e3522fe7d7204c3035806a87b7a652e69a6
ambnc <ambncfiable@gmail.com>
2026-05-19 01:56:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,11 @@
-const CACHE_NAME = "bet-guide-pwa-v2";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return Promise.all(
-        FILES_TO_CACHE.map(file => {
-          return cache.add(file).catch(error => {
-            console.log("Cache failed for:", file, error);
-          });
-        })
-      );
-    })
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") {
-    return;
-  }
-
-  const requestUrl = new URL(event.request.url);
-
-  if (requestUrl.pathname.includes("favicon.ico")) {
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request).catch(() => {
-        return caches.match("./index.html");
-      });
-    })
+      return Promise.all(keys.map(key => caches.delete(key)));
+    }).then(() => self.clients.claim())
   );
 });
